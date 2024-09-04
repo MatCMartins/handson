673e09965afa5849b462e34c599aa368e10e3d19
mateus <22.01082-3@maua.br>
2024-09-03 23:02:02 -0300

    mudanças iniciais de design e tratativas de erros

--- sensor.py
+++ sensor.py
@@ -3,35 +3,32 @@
 #        Engenharia de Computação:      Angelo Zanini / Nuncio Perrella
 #        Ciência da Computação:         Ana Paula Serra / Angelo Zanini / Nuncio Perrella / Mateus Capaldo
 #        Design:                        Tiago Perrella
+
 import serial.tools.list_ports
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.image as mpimg
 
 def listar_portas_seriais():
     portas = serial.tools.list_ports.comports()
-    for porta in portas:
-        print(porta)
+    return portas[0].name
 
-if __name__ == "__main__":
-    listar_portas_seriais()
-
-
-
-# Configuração da porta serial
-#ser = serial.Serial('COM11', 115200)  # Substitua 'COM3' pela porta serial correta
 ser = serial.Serial(          
-    port='COM3',
+    port=listar_portas_seriais(),
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
 )
 
-
+# url = 'https://maua.br/images/logo-IMT.png'
+# image = mpimg.imread('https://maua.br/images/logo-IMT.png')
 
 # Configuração do gráfico
 fig, ax = plt.subplots()
+fig.patch.set_facecolor("#004785")
+# fig.figimage(image, 50, 400, zorder=1) 
 xs = []
 ys = []
 
@@ -39,8 +36,19 @@ def animate(i, xs, ys):
     # Ler uma linha da porta serial
     line = ser.readline().decode('utf-8').strip()
     # Adicionar os dados lidos nas listas
-    xs.append(len(xs))
-    ys.append(float(line))
+
+    try:
+        xs.append(len(xs))
+        if line == "Couldn't get distance: 0":
+            raise Exception
+        ys.append(float(line))
+        print(line)
+    except ValueError as err:
+        print(f"Inicializando conexão: {err}")
+        ys.append(0)
+    except Exception as err:
+        print(f"Valor inválido encontrado: {err}")
+        ys.append(sum(ys[len(ys) - 11: len(ys) - 1])/len(ys[len(ys) - 11: len(ys) - 1]))
 
     # Limitar o tamanho das listas para manter o gráfico atualizado
     xs = xs[-100:]
@@ -48,13 +56,22 @@ def animate(i, xs, ys):
 
     # Limpar e atualizar o gráfico
     ax.clear()
-    ax.plot(xs, ys)
+    ax.plot(xs, ys, color='white')
 
     # Configurar os eixos
-    ax.set_xlabel('Tempo [ms]')
-    ax.set_ylabel('Medida [mm]')
-    ax.set_title('Sensor de Altura  - Mauá')
+    ax.set_xlabel('Tempo [ms]', color='white')
+    ax.set_ylabel('Medida [mm]', color='white')
+    ax.set_title('Sensor de Altura  - Mauá', color='white')
+
+    ax.spines['bottom'].set_color('white')  
+    ax.spines['left'].set_color('white')  
+    ax.spines['right'].set_color('white')  
+    ax.spines['top'].set_color('white') 
+
+    ax.tick_params(axis='x', colors='white') 
+    ax.tick_params(axis='y', colors='white')
    
+    ax.set_facecolor("#004785")
    
 # Configuração da animação
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=100)
